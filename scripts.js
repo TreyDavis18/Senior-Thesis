01a8faed21b4eae16b61f2c18aa0163dc7a05ff1
TreyDavis18 <trey.davis0018@gmail.com>
2022-04-05 15:14:04 -0500

    Updates made in class

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
-const card = document.querySelector('.card__inner');
+//Function for card flip
+const card = document.querySelectorAll('.card__inner');
 
-card.addEventListener('click', function () {
+card.forEach((card) => card.addEventListener('click', function () {
     card.classList.toggle('is-flipped');
-});
+}));
