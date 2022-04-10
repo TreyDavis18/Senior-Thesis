625b9410b48cb91262e295fc9f14d605d8434da3
TreyDavis18 <trey.davis0018@gmail.com>
2022-04-10 18:57:41 -0500

    Beginnings of Parallax

--- scripts.js
+++ scripts.js
@@ -4,3 +4,5 @@ const card = document.querySelectorAll('.card__inner');
 card.forEach((card) => card.addEventListener('click', function () {
     card.classList.toggle('is-flipped');
 }));
+
+//Function for parallax
